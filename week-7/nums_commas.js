5de47869314f78628c6fc6265274a13c3a71edbe
Matt Harris <matrharr@gmail.com>
2016-01-20 15:04:56 -0800

    Add reflections

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -63,3 +63,7 @@ function separateComma(num) {
 separateComma(10000000)
 
 // Reflection
+// What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently? I approached it similarly but ran into issues. there seemed to be some problems converting data types as I wanted to so had to adjust, and it also didn't allow us to iterate with negative indexes.
+// What did you learn about iterating over arrays in JavaScript? - it looks like you cannot iterate from negative indexes.
+// What was different about solving this problem in JavaScript? - syntax, obviously and couldn't iterate from negative indexes.
+// What built-in methods did you find to incorporate in your refactored solution? - reverse, String, join, split, splice
